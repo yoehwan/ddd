09e730b746f5a9b4fee0a229b0e9e0372a316a8b
han <yoehwan@gmail.com>
2022-11-09 09:16:17 +0800

    :art: Feat: Start Project

--- bin/ddd.dart
+++ bin/ddd.dart
@@ -1,5 +1,24 @@
-import 'package:ddd/ddd.dart' as ddd;
+import 'package:ddd/exception.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as html show parse;
 
-void main(List<String> arguments) {
-  print('Hello world: ${ddd.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  if (arguments.isEmpty) {
+    throw ArgumentError();
+  }
+  final url = arguments.first;
+  if (!isURl(url)) {
+    throw URLValidExection();
+  }
+  final res = await http.get(Uri.parse(arguments.first));
+  if (res.statusCode == 200) {
+    String htmlToParse = res.body;
+    final doc = html.parse(htmlToParse);
+  }
+}
+
+bool isURl(String url) {
+  return RegExp(
+          r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
+      .hasMatch(url);
 }
---
+++ lib/exception.dart
@@ -0,0 +1,1 @@
+class URLValidExection implements Exception {}
